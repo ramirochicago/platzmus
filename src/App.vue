e24d164c24561ac97147ddcb19767edf530c0484
Ramiro <ramirochicago@gmail.com>
2017-04-02 12:21:29 -0300

    Fixed logo again

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template lang="pug">
   #app
-    img(src='https://ramirochicago.github.io/platzmus/assets/logo.png')
+    img(src='https://ramirochicago.github.io/platzmus/dist/logo.png')
     h1 PlatziMusic
     select(v-model="selectedCountry")
       option(v-for="country in countries" v-bind:value="country.value") {{ country.name }}
